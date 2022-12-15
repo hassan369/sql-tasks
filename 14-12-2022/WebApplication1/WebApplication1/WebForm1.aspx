3c9295fe7269a7742a4445ba60681c0141c0173d
hassan ayasrah <hassanayasrah68@gmail.com>
2022-12-15 10:14:41 +0300

    adding upload img task

---
+++ 14-12-2022/WebApplication1/WebApplication1/WebForm1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+
+        .img{
+            width:250px;
+            height:250px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Age"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Photo"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnUpload" Text="Upload" runat="server" OnClick="UploadFile" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <table class="auto-style1" id="table1" runat="server"><tr><td>image</td><td>name</td><td>age</td></tr>
+                <tr><td><asp:Image CssClass="img" ImageUrl="" ID="Image1" runat="server" /></td><td>&nbsp;</td><td>&nbsp;</td></tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 14-12-2022/WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=  DESKTOP-HIMQ0KV\\SQLEXPRESS; database=upload; integrated security=SSPI");
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("Select * from costmer", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+                // Iterating Data  
+                string table = "<table class=\"auto-style1\" id=\"table1\" runat=\"server\"><tr><td>image</td><td>name</td><td>age</td></tr>";
+
+                while (sdr.Read())
+                {
+                    table += $"<tr><td><img class=\"img\" src=\"{sdr[3]}\" ID=\"Image1\" runat=\"server\" /></td><td>{sdr[1]}</td><td>{sdr[2]}</td></tr>";
+                }
+
+                table += "</table>";
+                Label4.Text = table;
+            }
+            catch (Exception S)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + S);
+                Label1.Text = "OOPs, something went wrong.\n" + S;
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+
+
+        }
+
+
+        public void uploadToDatabase(string srcP)
+        {
+
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source = DESKTOP-HIMQ0KV\\SQLEXPRESS; database=upload; integrated security=SSPI");
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("insert into costmer  ( name, age, srcPath)values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + "Images/" + Path.GetFileName(FileUpload1.FileName) + "')", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                cm.ExecuteNonQuery();
+                // Displaying a message  
+                Console.WriteLine("Record Inserted Successfully");
+                Label1.Text = "Record Inserted Successfully";
+            }
+            catch (Exception A)
+            {
+                Console.WriteLine("OOPs, something went wrong." + A);
+                Label1.Text = "OOPs, something went wrong." + A;
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+
+
+
+        protected void UploadFile(object sender, EventArgs e)
+        {
+
+
+
+            
+            try
+            {
+                string folderPath = Server.MapPath("~/Images/");
+
+                //Check whether Directory (Folder) exists.
+                if (!Directory.Exists(folderPath))
+                {
+                    //If Directory (Folder) does not exists Create it.
+                    Directory.CreateDirectory(folderPath);
+                }
+                
+                //Save the File to the Directory (Folder).
+                string fullPath = folderPath + Path.GetFileName(FileUpload1.FileName);
+                string srcPath = "/Images/" + Path.GetFileName(FileUpload1.FileName);
+
+                FileUpload1.SaveAs(fullPath);
+                Image1.ImageUrl = srcPath;
+
+
+                uploadToDatabase(srcPath);
+
+
+
+
+                //Display the Picture in Image control.
+                //Image1.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+                //SqlConnection cc = new SqlConnection("data source = DESKTOP-V50HPE1\\SQLEXPRESS; database = batool ; integrated security=SSPI");
+
+                ////SqlCommand comand = new SqlCommand("select * from name_and_password", cc);
+                //cc.Open();
+                //string query = "insert into name_and_password (name,password) values ('" + TextBox1.Text + "','" + "Files/" + Path.GetFileName(FileUpload1.FileName) + "') ";
+                //SqlCommand cmd = new SqlCommand(query, cc);
+                //cmd.ExecuteNonQuery();
+                //cc.Close();
+            }
+            catch (SqlException q)
+            {
+                Response.Write(q.Message);
+            }
+
+        }
+
+
+
+    }
+}
